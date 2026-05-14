82f7bdd2d4d49037f496e9ada669d47cade9dbd8
Zwelihle Gininda <zweligini@gmail.com>
2026-05-14 11:40:22 +0200

    update API URL for production

--- frontend/app.py
+++ frontend/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-API_URL = "http://127.0.0.1:8000"
+API_URL = "https://appointment-system-api-twym.onrender.com"
 
 # -----------------------------
 # SESSION STATE INIT
